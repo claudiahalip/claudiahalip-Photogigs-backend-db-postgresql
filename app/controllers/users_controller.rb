67155fcf0fe7d85ce05b308851f96e9fe1d79bec
claudiahalip <claudia4ro@yahoo.com>
2020-12-01 09:09:46 -0600

    ussercontroller set

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,34 @@
+class UsersController < ApplicationController 
+    def index
+       @users = User.all 
+       if @users 
+        render json: {users: @users}
+       else 
+        render json: { status: 500, errors: ['There is no user']}
+       end
+    end
+    
+    def show
+       @user = User.find(params[:id])
+       if @user 
+        render json: {user: @user}
+       else
+        rende json: {status: 500, errors: ['user not found']}
+       end
+    end 
+
+    def create 
+        @user = User.new(user_params)
+        if @user.save
+            login!
+            render json: { status: 'user created'}
+        else
+            render json: {status: 500, errors: @user.errors.full_messages}
+        end
+    end
+    
+    private
+    def user_params
+        params.require(:user).permit(:username, :email, :password, :password_confirmation)
+    end
+end
