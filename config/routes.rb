bd9dd49f300be1f6bc6dd68353f315e4f3a5343f
claudiahalip <claudia4ro@yahoo.com>
2021-01-19 12:36:42 -0600

    first Heroku deploy

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  # root 'activities#index'
+   root 'activities#index'
   post '/login', to: 'sessions#create'
   delete '/logout', to: 'sessions#destroy'
   get 'logged_in', to: 'sessions#is_logged_in?'
